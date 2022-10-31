d0a412e1cf03c1964d3001f978417715e387f84a
professormatheuslobo <professormatheuslobo@gmail.com>
2022-10-31 14:23:33 -0300

    minimap e contador

--- Assets/Scripts/camera.cs
+++ Assets/Scripts/camera.cs
@@ -6,13 +6,11 @@ public class camera : MonoBehaviour
 {
 	public GameObject player;//o player será arrasta pra cá
 	private Vector3 offset;
-	// Use this for initialization
+	
 	void Start()
 	{
 		offset = transform.position - player.transform.position;
 	}
-
-	// Update is called once per frame
 	void LateUpdate()
 	{
 		transform.position = player.transform.position + offset;
